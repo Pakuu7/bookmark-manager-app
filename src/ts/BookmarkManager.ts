38edfb0bb50254c1656b9d689bd3817a708ad9b6
Pakuu7 <paweldudziak@spoko.pl>
2026-01-15 19:43:47 +0100

    feat: implement dynamic bookmark card rendering from JSON

---
+++ src/ts/BookmarkManager.ts
@@ -0,0 +1,116 @@
+import data from '../json/data.json'
+
+const cardsContainer = document.querySelector('.cards') as HTMLDivElement;
+
+interface Bookmark {
+    id: string;
+    title: string;
+    url: string;
+    favicon: string;
+    description: string;
+    tags: string[];
+    pinned: boolean;
+    isArchived: boolean;
+    visitCount: number;
+    createdAt: string;
+    lastVisited: string | null;
+}
+
+const bookmarks: Bookmark[] = data.bookmarks
+
+function formatDate(date: string | null) {
+        if (!date) return 'Never'
+        const dateObject = new Date(date)
+        const day = dateObject.getDate()
+        const month = dateObject.toLocaleDateString('en-US', {month: 'short'})
+        return `${day} ${month}`
+}
+
+export function showBookmarks() {
+    const fragment = document.createDocumentFragment()
+    bookmarks.forEach(bookmark => {
+        const card = document.createElement('div');
+        card.classList.add('bg-neutral-0', 'shadow-lg', 'rounded-xl')
+        const lastVisitedText = formatDate(bookmark.lastVisited);
+        const createdAtText = formatDate(bookmark.createdAt)
+        const displayUrl = bookmark.url
+            .replace('https://', '')
+            .replace('http://', '')
+            .replace('www.', '')
+        let tagsHTML = ''
+        bookmark.tags.forEach(tag => {
+            tagsHTML += `<span class='tag text-preset-5 bg-neutral-100 px-3 py-1 rounded-sm'>${tag}</span>`
+        })
+
+        card.innerHTML = `
+        <div class="top flex justify-between items-start pb-3 p-4 border-b border-b-neutral-300">
+                <div class="flex gap-4 items-center">
+                    <img src="${bookmark.favicon}"
+                         class="w-12 h-12 rounded-lg border border-neutral-100"
+                         alt="${bookmark.title} favicon">
+                    <div>
+                        <h3 class="text-preset-2 text-neutral-900">${bookmark.title}</h3>
+                        <p class="text-preset-5 text-neutral-800">${displayUrl}</p>
+                    </div>
+                </div>
+                <button class="border border-neutral-400 p-1 rounded-lg cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"
+                         stroke="#001F1F" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"/>
+                    </svg>
+                </button>
+            </div>
+            <div class="pt-3 pb-3 p-4">
+                <p class="text-preset-4-medium">
+                    ${bookmark.description}
+                </p>
+            </div>
+            <div class="flex flex-wrap space-x-2 pb-3 pt-2 px-4 border-b border-b-neutral-300">
+                ${tagsHTML}
+            </div>
+            <div class="flex items-center justify-between text-neutral-800 px-4 py-3">
+                <div class="flex items-center space-x-4">
+                    <div class="flex items-center gap-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="none" viewBox="0 0 20 20">
+                            <path stroke="#001F1F" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.6"
+                                  d="M2.017 10.594c-.114-.18-.17-.27-.202-.408a1 1 0 0 1 0-.372c.032-.139.088-.229.202-.408.938-1.485 3.73-5.24 7.983-5.24 4.255 0 7.046 3.755 7.984 5.24.113.18.17.27.202.408a1 1 0 0 1 0 .372c-.032.139-.089.229-.202.408-.938 1.485-3.73 5.24-7.984 5.24s-7.045-3.755-7.983-5.24"/>
+                            <path stroke="#001F1F" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.6"
+                                  d="M10 12.5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
+                        </svg>
+                        <span class="text-preset-5">${bookmark.visitCount}</span>
+                    </div>
+                    <div class="flex items-center gap-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="none" viewBox="0 0 20 20">
+                            <g clip-path="url(#a)">
+                                <path stroke="#001F1F" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.6"
+                                      d="M10 5v5l3.334 1.667m5-1.667a8.333 8.333 0 1 1-16.667 0 8.333 8.333 0 0 1 16.667 0"/>
+                            </g>
+                            <defs>
+                                <clipPath id="a">
+                                    <path fill="#fff" d="M0 0h20v20H0z"/>
+                                </clipPath>
+                            </defs>
+                        </svg>
+                        <span class="text-preset-5">${lastVisitedText}</span>
+                    </div>
+                    <div class="flex items-center gap-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="none" viewBox="0 0 20 20">
+                            <path stroke="#001F1F" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.6"
+                                  d="M17.5 8.333h-15m10.833-6.666V5M6.667 1.667V5M6.5 18.333h7c1.4 0 2.1 0 2.635-.272a2.5 2.5 0 0 0 1.092-1.093c.273-.535.273-1.235.273-2.635v-7c0-1.4 0-2.1-.273-2.635a2.5 2.5 0 0 0-1.092-1.092c-.535-.273-1.235-.273-2.635-.273h-7c-1.4 0-2.1 0-2.635.273a2.5 2.5 0 0 0-1.093 1.092C2.5 5.233 2.5 5.933 2.5 7.333v7c0 1.4 0 2.1.272 2.635a2.5 2.5 0 0 0 1.093 1.093c.535.272 1.235.272 2.635.272"/>
+                        </svg>
+                        <span class="text-preset-5">${createdAtText}</span>
+                    </div>
+                </div>
+                <button class="text-gray-400 hover:text-gray-600 px-2 cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 20 20">
+                        <path stroke="#001F1F" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.6"
+                              d="M10 12.5v5.833M6.668 6.09v1.776c0 .173 0 .26-.017.343a.8.8 0 0 1-.074.211c-.039.076-.093.143-.201.279l-1.308 1.635c-.555.693-.832 1.04-.833 1.332 0 .254.115.494.314.652.228.182.672.182 1.56.182h7.785c.888 0 1.332 0 1.56-.182a.83.83 0 0 0 .314-.652c0-.292-.278-.639-.833-1.332l-1.308-1.635a1.8 1.8 0 0 1-.2-.279.8.8 0 0 1-.075-.211c-.017-.083-.017-.17-.017-.343V6.09c0-.096 0-.144.005-.191a1 1 0 0 1 .024-.125c.013-.045.03-.09.066-.18l.84-2.099c.245-.612.368-.919.317-1.165a.83.83 0 0 0-.356-.525c-.21-.138-.54-.138-1.199-.138H6.97c-.66 0-.99 0-1.2.138a.83.83 0 0 0-.355.525c-.05.246.072.553.317 1.165l.84 2.1c.035.089.053.134.066.18q.016.06.024.124c.005.047.005.095.005.191"/>
+                    </svg>
+                </button>
+            </div>
+     `
+        fragment.appendChild(card)
+    })
+    cardsContainer.appendChild(fragment)
+}
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,10 +1,14 @@
+import {showBookmarks} from "./BookmarkManager.ts";
+
 const addInput = document.querySelector('.add') as HTMLButtonElement;
 const addBookmarkPanel = document.querySelector('.add-bookmark') as HTMLDivElement;
 const closeBookmarkPanel = document.querySelector('.bookmarkClose') as HTMLButtonElement;
+// const addBookmarkBtn = document.querySelector('.addBookmarkBtn') as HTMLButtonElement;
 
 function showAddPanel() {
     addBookmarkPanel.classList.toggle('hidden')
 }
-
 addInput.addEventListener('click', showAddPanel)
 closeBookmarkPanel.addEventListener('click', showAddPanel)
+
+showBookmarks()
