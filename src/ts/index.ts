7c11a237fe98fecd2144018d9e8d2726e89d3c00
Pakuu7 <paweldudziak@spoko.pl>
2026-01-20 19:06:36 +0100

    feat: implement sort-menu structure and style

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,11 +1,12 @@
-import {showBookmarks} from "./BookmarkManager.ts";
+import {showBookmarks} from "./bookmark-manager.ts";
+// import {sortBookmarks} from "./sorting.ts";
 
 const addInput = document.querySelector('.add') as HTMLButtonElement;
 const addBookmarkPanel = document.querySelector('.add-bookmark') as HTMLDivElement;
 const closeBookmarkPanel = document.querySelector('.bookmarkClose') as HTMLButtonElement;
 // const addBookmarkBtn = document.querySelector('.addBookmarkBtn') as HTMLButtonElement;
 const closeHamburgerMenuBtn = document.querySelector('.close-hamburger-btn') as HTMLImageElement;
-const openHamburgerMenuBtn = document.querySelector('.open-hamburguer-btn') as HTMLButtonElement;
+const openHamburgerMenuBtn = document.querySelector('.open-hamburger-btn') as HTMLButtonElement;
 
 const hamburgerMenu = document.querySelector('.hamburger-menu') as HTMLDivElement;
 
@@ -19,3 +20,4 @@ closeHamburgerMenuBtn.addEventListener('click', () => toggleElement(hamburgerMen
 openHamburgerMenuBtn.addEventListener('click', () => toggleElement(hamburgerMenu))
 
 showBookmarks()
+// sortBookmarks()
---
+++ src/ts/sorting.ts
@@ -0,0 +1,13 @@
+// import data from '../json/data.json'
+//
+// interface Bookmark {
+//     createdAt: string;
+//     lastVisited: string | null;
+//     visitCount: number;
+// }
+//
+// const bookmarks: Bookmark[] = data.bookmarks
+//
+// export function sortBookmarks() {
+//     console.log(bookmarks)
+// }
