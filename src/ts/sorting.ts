f487a623e85b556e4777762968599b1df208dcbb
Pakuu7 <paweldudziak@spoko.pl>
2026-01-24 14:45:43 +0100

    feat: add recently visited option and refactor logic

--- src/ts/sorting.ts
+++ src/ts/sorting.ts
@@ -3,16 +3,19 @@ import {showBookmarks} from "./bookmark-manager.ts";
 import type {Bookmark} from "./bookmark-manager.ts";
 
 const recentlyAddedBtn = document.querySelector('.recently-added') as HTMLButtonElement;
+const recentlyVisitedBtn = document.querySelector('.recently-visited') as HTMLButtonElement
 
 const bookmarks: Bookmark[] = data.bookmarks
 
 export function sortBookmarks() {
-    recentlyAddedBtn.addEventListener('click', () => {
-        const sortedBookmarks = [...bookmarks].sort((a, b) => {
-            const dateA = new Date(a.createdAt ?? 0).getTime();
-            const dateB = new Date(b.createdAt ?? 0).getTime();
+    const handleSort = (property: 'createdAt' | 'lastVisited') => {
+        const sorted = [...bookmarks].sort((a, b) => {
+            const dateA = new Date(a[property] ?? 0).getTime()
+            const dateB = new Date(b[property] ?? 0).getTime()
             return dateB - dateA
         })
-        showBookmarks(sortedBookmarks)
-    })
+        showBookmarks(sorted)
+    }
+    recentlyAddedBtn.addEventListener('click', () => handleSort('createdAt'))
+    recentlyVisitedBtn.addEventListener('click', () => handleSort('lastVisited'))
 }
