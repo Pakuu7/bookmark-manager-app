b5a112969d3cfa3ea79da17bef251dcb408a1c02
Pakuu7 <paweldudziak@spoko.pl>
2026-01-24 15:04:44 +0100

    feat: add visitCount option and unify sorting logic

--- src/ts/sorting.ts
+++ src/ts/sorting.ts
@@ -4,18 +4,27 @@ import type {Bookmark} from "./bookmark-manager.ts";
 
 const recentlyAddedBtn = document.querySelector('.recently-added') as HTMLButtonElement;
 const recentlyVisitedBtn = document.querySelector('.recently-visited') as HTMLButtonElement
+const mostVisitedBtn = document.querySelector('.most-visited') as HTMLButtonElement
 
 const bookmarks: Bookmark[] = data.bookmarks
 
 export function sortBookmarks() {
-    const handleSort = (property: 'createdAt' | 'lastVisited') => {
+    const handleSort = (property: 'createdAt' | 'lastVisited' | 'visitCount') => {
         const sorted = [...bookmarks].sort((a, b) => {
-            const dateA = new Date(a[property] ?? 0).getTime()
-            const dateB = new Date(b[property] ?? 0).getTime()
-            return dateB - dateA
+            const valA = a[property]
+            const valB = b[property]
+
+            if (property === 'visitCount') {
+                return (valB as number) - (valA as number)
+            }
+
+            const timeA = new Date(valA as string ?? 0).getTime()
+            const timeB = new Date(valB as string ?? 0).getTime()
+            return timeB - timeA
         })
         showBookmarks(sorted)
     }
     recentlyAddedBtn.addEventListener('click', () => handleSort('createdAt'))
     recentlyVisitedBtn.addEventListener('click', () => handleSort('lastVisited'))
+    mostVisitedBtn.addEventListener('click', () => handleSort('visitCount'))
 }
